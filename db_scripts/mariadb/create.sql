2957fed947c2a464061457f4b45f268e6775afd2
philippe.dubois@alfresco.com <philippe.dubois@alfresco.com>
2015-06-25 22:01:32 +0200

    removed "exit;" from db creation scripts

--- db_scripts/mariadb/create.sql
+++ db_scripts/mariadb/create.sql
@@ -2,4 +2,4 @@ show databases;
 create database alfresco default character set utf8 collate utf8_bin;
 grant all on alfresco.* to 'alfresco'@'%' identified by 'alfresco' with grant option;
 show databases;
-exit;
+
--- db_scripts/mysql/create.sql
+++ db_scripts/mysql/create.sql
@@ -1,3 +1,3 @@
 create database IF NOT EXISTS alfresco default character set utf8 collate utf8_bin;
 grant all on alfresco.* to 'alfresco'@'%' identified by 'alfresco' with grant option;
-exit;
+
